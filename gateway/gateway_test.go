d0b2e8c02183e24ba340822f44c70b3b6d5376f0
idada <bg5sbk@gmail.com>
2016-09-26 21:34:52 +0800

    完善单元测试

--- gateway/gateway_test.go
+++ gateway/gateway_test.go
@@ -3,6 +3,7 @@ package gateway
 import (
 	"math/rand"
 	"net"
+	"sync"
 	"testing"
 	"time"
 
@@ -19,7 +20,7 @@ func Test_Gateway(t *testing.T) {
 
 	gw := NewGateway(TestPool, 2048)
 
-	go gw.ServeClients(lsn1, 10, 1024, 1024, 30)
+	go gw.ServeClients(lsn1, 10000, 1024, 1024, 30)
 	go gw.ServeServers(lsn2, "123", 3, 1024, 1024, 30)
 
 	time.Sleep(time.Second)
@@ -30,33 +31,62 @@ func Test_Gateway(t *testing.T) {
 	server, err := DialServer(lsn2.Addr().String(), TestMsgFormat{}, TestPool, 123, "123", 3, 2048, 1024, 1024)
 	utest.IsNilNow(t, err)
 
-	var vconns [2]*link.Session
-	var connIDs [2]uint32
+	var vconns [2][2]*link.Session
+	var connIDs [2][2]uint32
+	var clientID uint32
 
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
 		var err error
-		vconns[0], connIDs[0], _, err = server.Accept()
+
+		vconns[0][0], connIDs[0][0], clientID, err = server.Accept()
 		utest.IsNilNow(t, err)
+
+		vconns[1][0], connIDs[1][0], _, err = client.Accept()
+		utest.IsNilNow(t, err)
+
+		wg.Done()
 	}()
 
-	vconns[1], connIDs[1], err = client.Dial(123)
+	vconns[0][1], connIDs[0][1], err = client.Dial(123)
 	utest.IsNilNow(t, err)
 
-	utest.EqualNow(t, connIDs[0], connIDs[1])
+	vconns[1][1], connIDs[1][1], err = server.Dial(clientID)
+	utest.IsNilNow(t, err)
 
-	for i := 0; i < 10000; i++ {
+	wg.Wait()
+
+	utest.EqualNow(t, connIDs[0][0], connIDs[0][1])
+	utest.EqualNow(t, connIDs[1][0], connIDs[1][1])
+
+	n := 5000 + rand.Intn(10000)
+
+	for i := 0; i < n; i++ {
 		buffer1 := make([]byte, 1024)
 		for i := 0; i < len(buffer1); i++ {
 			buffer1[i] = byte(rand.Intn(256))
 		}
 
 		x := rand.Intn(2)
+		y := rand.Intn(2)
 
-		err := vconns[x].Send(buffer1)
+		err := vconns[x][y].Send(buffer1)
 		utest.IsNilNow(t, err)
 
-		buffer2, err := vconns[(x+1)%2].Receive()
+		buffer2, err := vconns[x][(y+1)%2].Receive()
 		utest.IsNilNow(t, err)
 		utest.EqualNow(t, buffer1, buffer2)
+	}
+
+	vconns[0][0].Close()
+	vconns[0][1].Close()
+	vconns[1][0].Close()
+	vconns[1][1].Close()
+
+	time.Sleep(time.Second)
+
+	for i := 0; i < len(gw.virtualConns); i++ {
+		utest.EqualNow(t, 0, len(gw.virtualConns[i]))
 	}
 }
