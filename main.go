cfbe401d59b7169ba6f222e3fc46b036e44b95e4
idada <bg5sbk@gmail.com>
2016-09-28 23:17:40 +0800

    完善代码

--- main.go
+++ main.go
@@ -13,28 +13,26 @@ import (
 )
 
 var (
-	reusePort       = flag.Bool("ReusePort", false, "Enable/disable the reuseport feature.")
+	reusePort       = flag.Bool("ReusePort", false, "Enable/Disable the reuseport feature.")
 	maxPacketSize   = flag.Int("MaxPacketSize", 512*1024, "Limit max packet size.")
-	memPoolType     = flag.String("MemPoolType", "atom", "Type of memory pool (sync | atom | chan).")
+	memPoolType     = flag.String("MemPoolType", "atom", "Type of memory pool ('sync', 'atom' or 'chan').")
 	memPoolSize     = flag.Int("MemPoolSize", 10*1024*1024, "Size of memory pool.")
 	memPoolFactor   = flag.Int("MemPoolFactor", 2, "Growth in chunk size of memory pool.")
 	memPoolMinChunk = flag.Int("MemPoolMinChunk", 64, "Smallest chunk size of memory pool.")
 	memPoolMaxChunk = flag.Int("MemPoolMaxChunk", 64*1024, "Largest chunk size of memory pool.")
 
-	clientAddr         = flag.String("ClientAddr", ":0", "Is the address where clients connect to.")
+	clientAddr         = flag.String("ClientAddr", ":0", "The gateway address where clients connect to.")
 	clientMaxConn      = flag.Int("ClientMaxConn", 8, "Limit max virtual connections for each client.")
 	clientBufferSize   = flag.Int("ClientBufferSize", 2*1024, "Setting bufio.Reader's buffer size.")
 	clientSendChanSize = flag.Int("ClientSendChanSize", 1024, "Tunning client session's async behavior.")
-	clientPingInterval = flag.Duration("ClientPingInterval", 30*time.Second,
-		"The seconds of that gateway not receiving message from client will send PING command to check it alive.")
+	clientPingInterval = flag.Duration("ClientPingInterval", 30*time.Second, "The interval of that gateway sending PING command to client.")
 
-	serverAddr         = flag.String("ServerAddr", ":0", "Is the address where servers connect to.")
+	serverAddr         = flag.String("ServerAddr", ":0", "The gateway address where servers connect to.")
 	serverAuthTimeout  = flag.Int("ServerAuthTimeout", 3, "Server auth IO waiting timeout.")
 	serverAuthKey      = flag.String("ServerAuthKey", "", "The private key used to auth server connection.")
 	serverBufferSize   = flag.Int("ServerBufferSize", 64*1024, "Buffer size of bufio.Reader for server connections.")
 	serverSendChanSize = flag.Int("ServerSendChanSize", 102400, "Tunning server session's async behavior, this value must be greater than zero.")
-	serverPingInterval = flag.Duration("ServerPingInterval", 30*time.Second,
-		"The seconds of that gateway not receiving message from server will send PING command to check it alive.")
+	serverPingInterval = flag.Duration("ServerPingInterval", 30*time.Second, "The interval of that gateway sending PING command to server.")
 )
 
 func main() {
